d0b9b509f3528b237bfe81f6798a9877b062b3e6
Jameswain <Jameswain@163.com>
2019-09-02 00:02:04 +0800

    修改mock文件测试

--- example/src/demo04/mock-data/other.js
+++ example/src/demo04/mock-data/other.js
@@ -5,7 +5,7 @@ const Mock = require('mockjs');
 
 //  mock配置文件，key就是接口的URL地址，value可以是对象，或者函数，函数更灵活，函数有三个参数，分别是：请求参数，request对象，response对象
 module.exports = {
-    enable: true,              // 全局mock开关，如果不写，默认为开启状态， 如果设置为false，表示关闭整个配置文件的mock配置，等服务端的接口准备ready后，可以将这个字段设置为false
+    // enable: false,              // 全局mock开关，如果不写，默认为开启状态， 如果设置为false，表示关闭整个配置文件的mock配置，等服务端的接口准备ready后，可以将这个字段设置为false
     '/search_subjects': (params, req, res) => {
         // console.log('/search_subjects =>', params);
         return {
